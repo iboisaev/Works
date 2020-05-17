e9b0648a02a74124b69fdf37a6ebbb4fc8c8d0ab
iboisaev <61871875+iboisaev@users.noreply.github.com>
2020-05-17 15:41:39 +0400

    Add files via upload

--- delivery/js/main.js
+++ delivery/js/main.js
@@ -1,18 +1,27 @@
- const shoppingCard = document.querySelector('.shoppingCard');
- const modal = document.querySelector('.modal');
- const close = document.querySelector('.modal__close');
- const lockedBody = document.getElementsByTagName('body')[0];
+	// переменные, используемые при открытии и закрытии корзины
+const shoppingCard = document.querySelector('.shoppingCard'),
+      modal = document.querySelector('.modal'),
+      close = document.querySelector('.modal__close'),
+      cancel = document.querySelector('.modal__cancel'),
+      lockedBody = document.getElementsByTagName('body')[0];
 
-	// открытие модального окна при клине на кнопку "Корзина"
- shoppingCard.addEventListener('click', function(event) {
- 	modal.classList.add('is-open');
- 	lockedBody.classList.add('locked');
- });
 
-	// закрытие модального окна при клине на крестик
- close.addEventListener('click', function(event) {  
+	// открытие корзины при клине на кнопку "Корзина"
+shoppingCard.addEventListener('click', function(event) {
+	modal.classList.add('is-open');
+	lockedBody.classList.add('locked');
+});
+
+	// закрытие корзины при клине на "Крестик"
+close.addEventListener('click', function(event) {  
+	modal.classList.remove('is-open');
+	lockedBody.classList.remove('locked');
+});
+
+ 	// закрытие корзины при клине на кнопку "Отменить"
+cancel.addEventListener('click', function(event) {  
  	modal.classList.remove('is-open');
  	lockedBody.classList.remove('locked');
- });
+});
 
- new WOW().init();
+new WOW().init();
